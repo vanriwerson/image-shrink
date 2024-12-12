469a710c8601383977ba827df5e2c821bd665108
Bruno Riwerson Silva <riwerson@gmail.com>
2024-12-12 09:03:01 -0300

    feat: adicionando ícones e aplicando caminho absoluto

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ let mainWindow
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`, // utilizar caminho absoluto para o ícone
     title: 'ImageShrink',
     width: 500
   })
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`) // carrega o index.html mas precisa da informação do protocolo 'file://'
-  mainWindow.loadFile('./app/index.html') // carrega o index.html diretamente
+  mainWindow.loadFile(`${__dirname}/app/index.html`) // carrega o index.html diretamente
 }
 
 app.on('ready', createMainWindow)
